9bc200ee04f84f31eef1a60c7c7f08c023fe5e67
richbm10 <ricardo.bonilla.morales@gmail.com>
2024-07-31 23:49:26 -0600

    Refactor SaprissaCentenoCollection from ERC721 to a more simple ERC1155 solution to reduce gass fees

--- contracts/SaprissaCentenoCollection.sol
+++ contracts/SaprissaCentenoCollection.sol
@@ -2,92 +2,34 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract SaprissaCentenoCollection is
-    ERC721,
-    ERC721URIStorage,
-    ERC721Enumerable,
+    ERC1155,
     Ownable,
-    ERC721Pausable,
-    ERC721Burnable
+    ERC1155Pausable,
+    ERC1155Burnable,
+    ERC1155Supply
 {
     uint256 public constant MAX_SUPPLY = 150;
-    uint256 private _currentTokenId = 0;
 
     constructor(
-        address initialOwner,
-        string[] memory tokenURIs
-    ) ERC721("SaprissaCentenoCollection", "CENTENO") Ownable(initialOwner) {
-        require(tokenURIs.length <= MAX_SUPPLY, "Too many URIs");
-
-        for (uint256 i = 0; i < tokenURIs.length; i++) {
-            _safeMint(initialOwner, _currentTokenId);
-            _setTokenURI(_currentTokenId, tokenURIs[i]);
-            _currentTokenId++;
-        }
-    }
-
-    modifier onlyOwnerOrApproved() {
-        require(
-            msg.sender == owner() || isApprovedForAll(owner(), msg.sender),
-            "Caller is not owner nor approved"
-        );
-        _;
-    }
-
-    function setApprovalForAll(
-        address operator,
-        bool approved
-    ) public override(ERC721, IERC721) onlyOwner {
-        super.setApprovalForAll(operator, approved);
-    }
-
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function _increaseBalance(
-        address account,
-        uint128 amount
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, amount);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
+        address initialOwner
     )
-        internal
-        override(ERC721, ERC721Enumerable, ERC721Pausable)
-        returns (address)
+        ERC1155(
+            "https://ipfs.io/ipfs/QmXnoLSNsTYutkqC6JwoZ7HojyutfKwPMtndtsejYWMBwm"
+        )
+        Ownable(initialOwner)
     {
-        return super._update(to, tokenId, auth);
+        mint(initialOwner, 1, MAX_SUPPLY, "");
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://mafi.onelink.com/ja33";
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
     }
 
     function pause() public onlyOwner {
@@ -96,5 +38,34 @@ contract SaprissaCentenoCollection is
 
     function unpause() public onlyOwner {
         _unpause();
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values
+    ) internal override(ERC1155, ERC1155Pausable, ERC1155Supply) {
+        super._update(from, to, ids, values);
     }
 }
