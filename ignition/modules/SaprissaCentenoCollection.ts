14a6a3f2dc7e7ded1f647c7f6c529bce0c03764c
richbm10 <ricardo.bonilla.morales@gmail.com>
2024-07-31 23:47:53 -0600

    Prepare SaprissaCentenoCollection NFT ignition script

---
+++ ignition/modules/SaprissaCentenoCollection.ts
@@ -0,0 +1,15 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const SaprissaCentenoCollectionModule = buildModule(
+  "SaprissaCentenoCollectionModule",
+  (m) => {
+    const saprissaCentenoCollectionModule = m.contract(
+      "SaprissaCentenoCollection",
+      ["0x9E17D83De550DFD4585632E725fD92E72b9C3f4a"]
+    );
+
+    return { saprissaCentenoCollectionModule };
+  }
+);
+
+export default SaprissaCentenoCollectionModule;
